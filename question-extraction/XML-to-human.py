72b50ee4381402a4a71e382d483120dcc94c7c3d
cristian-vijelie <cristianvijelie@gmail.com>
2020-07-31 12:25:13 +0300

    Added MXML to human-friendly parser

---
+++ question-extraction/XML-to-human.py
@@ -0,0 +1,44 @@
+import sys, getopt
+import xml.etree.ElementTree as ET
+
+def main(argv):
+    infile_name = ''
+    outfile_name = ''
+
+    try:
+        opts, _ = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            infile_name = arg
+        elif opt in ("-o", "--ofile"):
+            outfile_name = arg
+
+    outfile = open(outfile_name, "w")
+
+    tree = ET.parse(infile_name)
+    root = tree.getroot()
+
+    for element in root.iter('question'):
+        if element.attrib['type'] == 'multichoice':
+            name = element[1][0].text
+            answers = ""
+            for answer in element.iter('answer'):
+                if float(answer.attrib['fraction']) > 0:
+                    answers += ('+ ' + answer[0].text + '\n')
+                else:
+                    answers += ('- ' + answer[0].text + '\n')
+
+            outfile.write(name + '\n')
+            outfile.write(answers + '\n')
+        
+    
+    outfile.close()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
