6f57492e93528113c017d5f7275b11b32a71e302
Costin-Robert Sin <sin.costinrobert@gmail.com>
2020-08-09 20:29:17 +0300

    quiz-generator: add mongodb database connect functions
    Implement getCollection function that connects to a MongoDB
    database collection
    
    Implement submitQuery function that submits a query to a MongoDB
    database collection and returns a list of queried documents.
    
    Add more connection settings to config.json
    
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

--- quiz-generator/generate-quiz.py
+++ quiz-generator/generate-quiz.py
@@ -1,4 +1,5 @@
 import json
+import pymongo
 
 
 def openJsonConfig():
@@ -7,7 +8,32 @@ def openJsonConfig():
     return conf
 
 
+def getCollection(db_ip, db_name, db_collection):
+    client = pymongo.MongoClient(db_ip)
+    col = client[db_name][db_collection]
+    return col
+
+
+def submitQuery(collection, query):
+    response = []
+    cursor = collection.find(query)
+    for document in cursor:
+        response.append(document)
+    return response
+
+
 if __name__ == '__main__':
     config = openJsonConfig()
-    print(config)
+    quiz_settings = config['quiz-settings']
+    db_settings = config['database-settings']
 
+    try:
+        question_collection = \
+            getCollection(db_settings['database-ip'], db_settings['database-name'], db_settings['question-collection'])
+    except:
+        print('\033[0;31m' + 'error: ' + '\033[0m' + 'Connection to question-collection failed. Check database settings.')
+    try:
+        test_collection = \
+            getCollection(db_settings['database-ip'], db_settings['database-name'], db_settings['test-collection'])
+    except:
+        print('\033[0;31m' + 'error: ' + '\033[0m' + 'Connection to test-collection failed. Check database settings.')
