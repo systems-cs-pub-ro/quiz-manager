15b8b11ee6e9d80bb6923efdaf7dd159a7dcf725
Costin-Robert Sin <sin.costinrobert@gmail.com>
2020-10-10 12:17:55 +0300

    quiz-generator: add logging for mongodb errors
    Add logging for mongodb connection errors.
    
    Remove duplicate code and improved coding style
    on some parts of the code.
    
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

--- quiz-generator/generate-quiz.py
+++ quiz-generator/generate-quiz.py
@@ -3,7 +3,10 @@ import random
 import datetime
 import json
 import pymongo
+import logging
 
+log_format = '%(levelname)s: %(message)s'
+logging.basicConfig(format=log_format)
 
 def openJsonConfig():
     """
@@ -89,24 +92,22 @@ def selectTags(questions, tags):
     return response
 
 
-def selectQuestions(questions, size, maxSize):
+def selectQuestions(questions, size, max_size):
     """
     Takes an array of questions, sorts them according to the creation
     date and returns a random selection of 'size' questions from a
-    pool created with the first 'maxSize' sorted questions.
+    pool created with the first 'max_size' sorted questions.
 
     :param questions: Array of questions stored in JSON
     :param size: Number of returned questions
-    :param maxSize: Size of the random-selection pool
+    :param max_size: Size of the random-selection pool
     :return: Filtered array of questions stored in JSON
     """
-    pool = []
-    selection = []
 
     pool = sorted(questions, key=lambda question: question["createdOn"], reverse=True)
 
-    if maxSize < len(pool):
-        selection = random.sample(pool[0:maxSize], size)
+    if max_size < len(pool):
+        selection = random.sample(pool[0:max_size], size)
     else:
         if size < len(pool):
             selection = random.sample(pool, size)
@@ -129,23 +130,6 @@ def selectYear(questions, year):
         if itr["lastUsed"].year == year:
             response.append(itr)
     return response
-
-def selectYear(questions, year):
-    """
-    Iterates through an array of questions and returns a list of
-    questions that have been last used in the specified year.
-
-    :param questions: Array of questions stored in JSON
-    :param year: A number that indicates the year to be filtered
-    :return: Filtered array of questions stored in JSON
-    """
-    response = []
-    for itr in questions:
-        if itr["lastUsed"].year == year:
-            response.append(itr)
-
-    return response
-
 
 def generateQuiz(questions, quiz_settings):
     """
@@ -195,15 +179,15 @@ if __name__ == '__main__':
             getCollection(db_settings['database-ip'], db_settings['database-name'],
                           db_settings['question-collection'])
     except:
-        print('\033[0;31m' + 'error: ' + '\033[0m' +
-              'Connection to question-collection failed. Check database settings.')
+        logging.error('Connection to question-collection failed. Check database settings.')
+        sys.exit(2)
     try:
         test_collection = \
             getCollection(db_settings['database-ip'], db_settings['database-name'],
                           db_settings['test-collection'])
     except:
-        print('\033[0;31m' + 'error: ' + '\033[0m' +
-              'Connection to test-collection failed. Check database settings.')
+        logging.error('Connection to test-collection failed. Check database settings.')
+        sys.exit(2)
 
     questions = submitQuery(question_collection, {})
 
