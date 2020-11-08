34741bbf07445a18ce3c21132db6efc7f858cff0
Andrei David <16004190+dandrei279@users.noreply.github.com>
2020-11-08 12:23:35 +0200

    Solved bug with topics to MoodleXML

--- quiz-generator/generate-quiz.py
+++ quiz-generator/generate-quiz.py
@@ -191,15 +191,14 @@ def json2mXML(jsonQuestion, quiz):
     tag = ElementTree.SubElement(tags, 'tag')
     tagText = ElementTree.SubElement(tag, 'text')
     tagText.text = 'Dificulty=' + str(jsonQuestion['difficulty'])
-    
+
     # adding other tags
     for jsonTag in jsonQuestion['tags']:
         if jsonTag['key']:
             tag = ElementTree.SubElement(tags, 'tag')
             tagText = ElementTree.SubElement(tag, 'text')
             tagText.text = jsonTag['key'] + ":" + ','.join(jsonTag['values'])
-    
-    
+
     # adding the answers in the file
     for jsonAnswer in jsonQuestion['answers']:
         answer = ElementTree.SubElement(question, 'answer')
@@ -232,12 +231,11 @@ if __name__ == '__main__':
 
     questions = submitQuery(question_collection, {})
 
-    quiz = questions
-    #quiz = generateQuiz(questions, quiz_settings)
+    quiz = generateQuiz(questions, quiz_settings)
 
     quizMXML = ElementTree.Element('quiz')
 
-    for question in questions:
+    for question in quiz:
         quizMXML = json2mXML(question, quizMXML)
 
     roughXML = ElementTree.tostring(quizMXML)
